cbafd5f6ebb6c623a2ce2b9be650bed54652ec52
Roman Sklenář <mail@romansklenar.cz>
2014-03-10 13:04:04 +0100

    Implemented minimum viable product

---
+++ README.md
@@ -0,0 +1,20 @@
+## StartupMap.cz
+
+
+### Ruby & Rails version
+Exact version of ruby is specified in `.ruby-version` file in root of project. Also specifiy ruby version in `Gemfile`.
+
+### System dependencies
+Make sure you have installed [Pow](http://pow.cx/) and [Heroku Toolbelt](https://toolbelt.heroku.com/) in your system.
+
+### Configuration & Setup
+Use bundler to install and maintaing gem dependencies.
+
+### Services (APIs, job queues, cache servers, search engines, etc.)
+TODO
+
+### Deployment instructions
+TODO
+
+### Issues
+We use [Github Issues](https://github.com/romansklenar/startupmap/issues) as an issue tracker. All related info is there.
---
+++ public/assets/javascripts/application.js
@@ -0,0 +1,119 @@
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g, function (a, b) { var r = o[b]; return typeof r === 'string' || typeof r === 'number' ? r : a; });
+};
+
+$(document).ready(function() {
+
+  function initialize_map() {
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(49.8037633,15.4749126),
+      scrollwheel: false,
+      panControl: false,
+      streetViewControl: false,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      },
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.Vm,
+        position: google.maps.ControlPosition.TOP_LEFT
+      }
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var markerCluster = new MarkerClusterer(map);
+    var infoWindow = new google.maps.InfoWindow();
+    var markers = [];
+
+    var image = 'assets/images/pin_normal.png';
+    var window_template = '<div class="startup"><div class="heading"><h4><a href="{url}" target="_blank">{name}</a><br><small>{address}</small></h4></div><hr><div class="body"><p>{description}</p></div></div>'
+    var menu_template   = '<a href="#" data-index="{index}" class="startup list-group-item">{name}</a>'
+
+    $.ajax({
+      url: "/feed.json", // "http://www.startupjobs.cz/map.php"
+      type: "GET",
+      dataType: "json",
+      crossDomain: true,
+      // xhrFields: { withCredentials: true },
+      data: {},
+      success: function (data) {
+        $(data).each(function() {
+          var position = new google.maps.LatLng(this.latutide, this.longitude);
+          var marker = new google.maps.Marker({ title: this.name, position: position, map: map, icon: image, data: this });
+
+          google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.setOptions({
+              content: window_template.supplant({ url: marker.data.url, name: marker.data.name, address: marker.data.address, description: marker.data.description }),
+              maxWidth: 280
+            });
+            infoWindow.open(map, marker);
+          });
+
+          var link = menu_template.supplant({ url: marker.data.url, name: marker.data.name, address: marker.data.address, index: markers.length });
+          $("#sidebar nav").append(link);
+
+          markers.push(marker);
+
+        });
+        var markerCluster = new MarkerClusterer(map, markers, { gridSize: 50 });
+
+        $('#sidebar #counter').text($('#sidebar nav a.startup').length);
+        $('#sidebar nav a.startup').on('click', function() {
+          focusStartup($(this).data('index'));
+          return false;
+        });
+
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        alert( 'Could not load data.' );
+      }
+    });
+
+    $('a#logo').click(function() {
+      map.setOptions(mapOptions);
+      return false;
+    });
+
+    function focusStartup(index) {
+      var marker = markers[index];
+      var address = [ marker.data.address, marker.data.city ].filter(function(e) { return $.trim(e) }).join(', ');
+
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          console.log("Located startup " + marker.data.name + " @ " + address)
+          map.setCenter(results[0].geometry.location);
+          map.fitBounds(results[0].geometry.viewport);
+        } else {
+          console.log("Couldn't locate startup " + marker.data.name + " @ " + address)
+          map.setCenter(marker.position);
+          map.setZoom(14);
+        }
+      });
+
+      infoWindow.setOptions({
+        content: window_template.supplant({ url: marker.data.url, name: marker.data.name, address: marker.data.address, description: marker.data.description }),
+        maxWidth: 280
+      });
+      infoWindow.open(map, marker);
+    };
+
+    function focusAddress(address) {
+      geocoder.geocode({ 'address': address }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          new google.maps.Marker({ position: results[0].geometry.location, map: map, icon: image });
+          map.setCenter(results[0].geometry.location);
+          map.fitBounds(results[0].geometry.viewport); // map.setZoom(14);
+        } else {
+          alert( 'Could not find specified address (' + status + ').' );
+        }
+      });
+    };
+
+  };
+
+  google.maps.event.addDomListener(window, 'load', initialize_map);
+
+});
